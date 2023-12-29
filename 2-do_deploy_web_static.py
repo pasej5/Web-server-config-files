174b9375fbfa98459e9669e470badecf2b533149
Jealous Matsikachando <pasej5@gmail.com>
2023-12-29 07:18:45 +0000

    web server config files

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script distributes an archive to your web servers,"""
+from fabric.contrib import files
+from fabric.api import env, put, run
+import os
+
+env.hosts = ['100.26.236.53', '107.22.144.34']
+
+
+def do_deploy(archive_path):
+    """Function for deploy"""
+    if not os.path.exists(archive_path):
+        return False
+
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
+    try:
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except Exception:
+        return False
